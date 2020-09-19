82022988d133859638094a64621e2c68567f82bb
Jerry Hanks <jerryhanksokafor@gmail.com>
2020-09-19 22:47:24 +0100

    Initial commit

---
+++ app/src/main/java/com/jerryokafor/tictactoe/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.jerryokafor.tictactoe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.jerryokafor.tictactoe.ui.TicTacToeTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TicTacToeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TicTacToeTheme {
+        Greeting("Android")
+    }
+}
